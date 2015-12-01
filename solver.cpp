5021532a34fd6ea4663f219d07f91c3e067bc462
Qingyang Chen <qingyang.chen@gmail.com>
2015-11-30 21:58:49 -0500

    adding solver

--- solver.cpp
+++ solver.cpp
@@ -1,10 +1,14 @@
 class Solver {
-  public:
-    Model *solve(Formula *formula);
+public:
+  Model *solve(Formula *formula);
+private:
+  Model *solveR(Formula *formula, Model *model);
 };
 
 Model *Solver::solve(Formula *formula) {
+  return solveR(formula, new Model());
+}
 
+Model *Solver::solveR(Formula *formula, Model *model) {
 
-  return NULL;
 }
